e18896f52ebcd0fd77de3fd16b57dce79317bf8c
rajke24 <jarek.andrzejewski99@gmail.com>
2020-04-12 21:06:56 +0200

    reorganize dirs

---
+++ python/6-kyu/duplicate_encoder.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+def duplicate_encode(word):
+    word = word.lower()
+    counter = Counter(word)
+    return ''.join('(' if counter[c] == 1  else ')' for c in word)
