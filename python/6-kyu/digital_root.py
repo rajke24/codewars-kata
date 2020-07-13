9dbcd6f6b5ddf9043d03c30458e8940bb45b8275
rajke24 <jarek.andrzejewski99@gmail.com>
2020-07-13 13:12:24 +0200

    added new solutions

--- python/6-kyu/digital_root.py
+++ python/6-kyu/digital_root.py
@@ -5,3 +5,7 @@ def digital_root(n):
 
 def digital_root2(n):
     return n if n < 10 else digital_root(sum(map(int,str(n))))
+
+def find_outlier(integers):
+    e = [i % 2 for i in integers]
+    return integers[e.index(0)] if integers.count(0) == 1 else integers[e.index(1)]
