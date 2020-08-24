ac0050678440895e55b5aaba588806b40eb00cdb
rajke24 <jarek.andrzejewski99@gmail.com>
2020-08-24 17:19:14 +0200

    add new kata

---
+++ python/5-kyu/rot13.py
@@ -0,0 +1,11 @@
+import string
+import codecs
+
+def rot13(msg):
+    rot13 = str.maketrans( 
+        "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
+        "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+    return msg.translate(rot13)
+
+def rot13_2(msg):
+    return codecs.encode(msg, 'rot13')
