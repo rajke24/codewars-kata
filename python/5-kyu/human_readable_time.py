85fffe0d53edaf39d7b5642b106a96027da01725
rajke24 <jarek.andrzejewski99@gmail.com>
2020-09-02 16:13:18 +0200

    edit human_readable_time.py - 5-kyu

--- python/5-kyu/human_readable_time.py
+++ python/5-kyu/human_readable_time.py
@@ -5,6 +5,13 @@ def make_readable(sec):
     seconds = sec - hours * 3600 - minutes * 60
     return f'{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}'
 
-# Best solution
+# Also quite nice solution
 def make_readable2(s):
+    hours = s / 3600
+    minutes = s / 60 % 60
+    seconds = s % 60
+    return f'{hours:02}:{minutes:02}:{seconds:02}'
+
+# Best solution
+def make_readable3(s):
     return '{:02}:{:02}:{:02}'.format(s / 3600, s / 60 % 60, s % 60)
