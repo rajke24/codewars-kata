6643c7e656cdb2d12486b0e13146c907a15e8364
rajke24 <jarek.andrzejewski99@gmail.com>
2020-09-02 17:08:16 +0200

    edit rgb_to_hex_conversion.py - 5-kyu

--- python/5-kyu/rgb_to_fex_conversion.py
+++ python/5-kyu/rgb_to_fex_conversion.py
@@ -1,4 +1,2 @@
 def rgb(r, g, b):
-    return ''.join(['{:02X}'.format(n) if 0 <= n <= 255 else ('00' if n < 0 else 'FF')  for n in (r,g,b)])
-
-print(rgb(-3,1,1))
+    return ''.join([f'{n:02X}' if 0 <= n <= 255 else ('00' if n < 0 else 'FF')  for n in (r,g,b)])
